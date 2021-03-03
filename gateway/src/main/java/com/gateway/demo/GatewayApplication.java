4b87719256733ce4a2a4e46019781be9b2900ecd
ljw-eagle <61191910+ljw-eagle@users.noreply.github.com>
2021-03-03 14:14:30 +0800

    2021.3.3

--- gateway/src/main/java/com/gateway/demo/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/demo/GatewayApplication.java
@@ -7,20 +7,20 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("path_route", r -> r.path("/csdn")
-                        .uri("www.baidu.com"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("path_route", r -> r.path("/csdn")
+//                        .uri("www.baidu.com"))
+//                .build();
+//    }
 
 }
 
